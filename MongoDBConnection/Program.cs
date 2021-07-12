c1e242ab8f6d5980f38167f4a4683dd99d866445
cloudsystems22 <cloudsystems22@gmail.com>
2021-07-12 09:11:20 -0300

    Adicionar arquivos de projeto.

---
+++ MongoDBConnection/Program.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace MongoDBConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new MongoClient(
+                "mongodb+srv://davidfico22:davidfico22@cluster0.mrtwa.mongodb.net/test");
+
+            var db = client.GetDatabase("sample_mflix");
+
+            var collection = db.GetCollection<BsonDocument>("movies");
+
+            var result = collection.Find("{title:'The Princess Bride'}").FirstOrDefault();
+
+            Console.WriteLine(result);
+        }
+    }
+}
